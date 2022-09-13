58aa04307b325bad9e1edee2d070456f5b383a37
codeBindHex <xhunter19192020@gmail.com>
2022-09-13 09:22:00 +0200

    FirstCommit

---
+++ book_store_app/src/redux/bookSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+
+const bookSlice = createSlice({
+    name: 'book',
+    initState: {books : null},
+    reducers: {
+        
+    }
+})
+
+export default bookSlice.reducer; 
